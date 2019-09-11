efdb95152f3eb8e3ee186baf85044f5c20cd447f
zhliuyi <2528790067@qq.com>
2019-09-11 22:15:49 +0800

    day4代码：ORM操作 站点

--- demo/app01/urls.py
+++ demo/app01/urls.py
@@ -1,5 +1,11 @@
 from django.urls import  path
 from .views import *
 urlpatterns = [
-    path("index/",index)
+    path("index/",index),
+    path("addperson/",addperson),
+    path("getperson/",getperson),
+    path("deleteperson/",deleteperson),
+    path("updateperson/",updateperson),
+    path('addonemore/', addonemore),
+    path('getonemore/', getonemore),
 ]
